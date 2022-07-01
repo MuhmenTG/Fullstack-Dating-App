b7c961194ef72880d9284c79f3883acb05c06200
MuhmenP <muhmenpk@gmail.com>
2022-07-01 23:57:37 +0200

    removing api key

--- sendEmail.php
+++ sendEmail.php
@@ -6,11 +6,11 @@ class Email
     public static function sendMail($to, $subject, $bdoy)
     {
         $email = new \SendGrid\Mail\Mail(); 
-        $email->setFrom("muhmenpk@gmail.com", "Aviation Dating LOVE");
+        $email->setFrom("");
         $email->setSubject($subject);
         $email->addTo($to, "Example User");
         $email->addContent("text/plain", $bdoy);
-        $sendgrid = new \SendGrid("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");
+        $sendgrid = new \SendGrid("");
         try {
             $response = $sendgrid->send($email);
             $response->statusCode() . "\n";
